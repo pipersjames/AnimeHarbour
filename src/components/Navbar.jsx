74280c85f3b52211db9cd296d30cca11c8b72ee3
james <131738190+pipersjames@users.noreply.github.com>
2023-12-12 00:41:57 +1000

    style: navigation bar
    updated navigation styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,34 @@
 import { NavLink } from "react-router-dom";
+import magnifyingGlassImage from "../assets/magnifying-glass.png"
 
 export default function Navbar () {
 
     return (
-        <nav className="navigation">
-            <h3 className="AppName">AnimeHarbour</h3>
-            <ul>
-                
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/trending">Trending</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/recent-updates">Recent Updates</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/search">Search</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/favourites">Favourites</NavLink>
-                </li>
-            </ul>
+        <nav className="Navigation">
+            <div className="AppNameContainer">
+                <NavLink className="AppName" id="HomeNavigation" to="/">AnimeHarbour</NavLink>
+            </div>
+            <div className="NavOptionsContainer">
+                <ul className="NavOptions">
+                    
+                    <li>
+                        
+                    </li>
+                    <li>
+                        <NavLink to="/trending">Trending</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/recent-updates">Recent Updates</NavLink>
+                    </li>
+                    
+                    <li>
+                        <NavLink to="/favourites">Favourites</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="EyeGlassContainer">
+                <NavLink to="/search"><img id="EyeGlass" src={magnifyingGlassImage} alt="Search" /></NavLink>
+            </div> 
         </nav>
     )
 }
