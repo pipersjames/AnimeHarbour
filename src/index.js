48d431261ccae2aa492dd7ea17d92227dab35645
lzhalloran <lzhalloran@gmail.com>
2023-12-12 17:59:43 +1000

    renamed AnimeDataProvider component to AnimeFavouritesProvider

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApiProvider } from "./contexts/ApiProvider";
 import { BrowserRouter } from 'react-router-dom';
-import { AnimeDataProvider } from "./contexts/AnimeDataProvider";
+import { AnimeFavouritesProvider } from "./contexts/AnimeFavouritesProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ApiProvider>      
-        <AnimeDataProvider>
+      <ApiProvider>
+        <AnimeFavouritesProvider>
           <App />
-        </AnimeDataProvider>
+        </AnimeFavouritesProvider>
       </ApiProvider>
     </BrowserRouter>
   </React.StrictMode>
