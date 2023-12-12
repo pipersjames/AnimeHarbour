5732b03f0af86655cf4cf01ec08fc51694a50654
james <131738190+pipersjames@users.noreply.github.com>
2023-12-12 21:59:38 +1000

    removal: commented code
    removed commented code

--- src/components/AnimeCards.jsx
+++ src/components/AnimeCards.jsx
@@ -30,31 +30,4 @@ export default function AnimeList(props) {
   );
 }
 
-// import React from "react";
-
-// export default function AnimeList(props) {
-//   const animeListData = props.anime.anime.data;
-
-//   if (!animeListData || animeListData.length === 0) {
-//     // Handle the case when animeListData is not available or empty
-//     return null;
-//   }
-
-//   return (
-//     <div>
-//       {animeListData.map((animeData) => {
-//         const { mal_id, genres, title, images } = animeData;
-//         const imageUrl = images?.webp?.large_image_url || images?.jpg?.large_image_url;
-
-//         return (
-//           <div key={mal_id}>
-//             <img src={imageUrl} alt={title} style={{ maxWidth: "100%" }} />
-//             <h2>{title}</h2>
-
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
   
